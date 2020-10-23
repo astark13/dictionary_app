eabb75bef4649f35967ae52cfd4621fb7385d055
astark13 <adrianstark13@yahoo.com>
2020-10-23 12:40:43 +0300

    App changes first letter to capital letter trying to find a match

--- thesaurus.py
+++ thesaurus.py
@@ -1,16 +1,19 @@
 import json
 from difflib import get_close_matches
 
-# Test #8 for Github webhook funtionality on remotehost
 # Import data from json format into a variable, as a dictionary
 data = json.load(open("data.json"))
 word = ((input("Enter the word you are loking for:")).lower())
+capital = word.title()
 
 # This function takes a word as input and compares it with the keys from the dictionary
 def search(word):
     # If the word can be found in the dictionary keys, the value corresponding to it will be returned
     if word in data.keys():
         return data[word]
+    # If te word isn't found, it tries to find a match turning the first letter in a capital letter
+    elif capital in data.keys():
+        return data[capital]
     #  "get_close_matches" function/method looks for the "word" against the dictionary keys "data.keys"; if it finds smth (the lenght of the item is >0) it prints it out
     elif len(get_close_matches(word, data.keys())) > 0:
         print ("Did you mean %s instead? If 'yes' type 'y', else type 'n':" % get_close_matches(word, data.keys())[0])
@@ -31,3 +34,5 @@ if type(definitions) == list:
         print (description)
 else:
     print(definitions)
+
+
